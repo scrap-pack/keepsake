ca09e921ba50d7fd2680cf26353b24320bca586c
Benson <ben72s5on@gmail.com>
2019-09-11 11:55:48 -0400

    Navbar now works with mobile display

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,55 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logoutThunk, changeLoginStatus } from '../redux/users';
 
-const Navbar = props => {
-  const { logout } = props;
-  const { authenticated } = props.currentUser;
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const amIAuthenticated = authenticated ? (
-    <div>
-      <li key="my-images">
-        <Link to="/scrapbook">My Images</Link>
-      </li>
-      <li key="logout">
-        <Link to="/login" onClick={logout}>
-          Logout
-        </Link>
-      </li>
-    </div>
-  ) : (
-    <div>
-      <li key="signup">
-        <Link to="/signup">Sign Up</Link>
-      </li>
-      <li key="login">
-        <Link to="/login">Login</Link>
-      </li>
-    </div>
-  );
+  componentDidMount() {
+    $(document).ready(function() {
+      $('.sidenav').sidenav();
+    });
+  }
 
-  return (
-    <nav className="teal darken-2" role="navigation">
-      <div className="nav-wrapper container">
-        <a id="logo-container" href="/" className="brand-logo">
-          <div id="logo" style={{ fontFamily: 'Chalkduster, fantasy' }}>
-            KeepSake
-          </div>
-        </a>
-        <ul className="right hide-on-med-and-down">{amIAuthenticated}</ul>
-        <ul id="nav-mobile" className="sidenav">
-          <li>
-            <a href="/">Navbar Link</a>
-          </li>
-        </ul>
-        <a href="#" data-target="nav-mobile" className="sidenav-trigger">
-          <i className="material-icons">menu</i>
-        </a>
+  render() {
+    const { logout } = this.props;
+    const { authenticated } = this.props.currentUser;
+
+    const amIAuthenticated = authenticated ? (
+      <div>
+        <li key="my-images">
+          <Link to="/scrapbook">My Images</Link>
+        </li>
+        <li key="logout">
+          <Link to="/login" onClick={logout}>
+            Logout
+          </Link>
+        </li>
       </div>
-    </nav>
-  );
-};
+    ) : (
+      <div>
+        <li key="signup">
+          <a href="/signup">SIGN UP</a>
+        </li>
+        <li key="login">
+          <a href="/login">LOGIN</a>
+        </li>
+      </div>
+    );
+
+    return (
+      <nav className="teal darken-2" role="navigation">
+        <div className="nav-wrapper container">
+          <a id="logo-container" href="/" className="brand-logo">
+            <div id="logo" style={{ fontFamily: 'Chalkduster, fantasy' }}>
+              KeepSake
+            </div>
+          </a>
+          <ul className="right hide-on-med-and-down">{amIAuthenticated}</ul>
+          <ul id="nav-mobile" className="sidenav">
+            {amIAuthenticated}
+          </ul>
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
 
 const mapState = ({ currentUser }) => ({ currentUser });
 const mapDispatch = dispatch => ({
